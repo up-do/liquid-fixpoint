988b5c377d1a5701c1f19147be998ecc71d28622
Phil de Joux <philderbeast@gmail.com>
2023-11-27 15:24:46 -0500

    Configure with updo

---
+++ project-versions.mk
@@ -0,0 +1,16 @@
+# Versions of GHC and stackage resolver, the ones we're on and the next ones
+# we're upgrading to.
+GHC_VERSION ?= 9.6.3
+STACKAGE_VERSION ?= nightly-2021-11-25
+
+# For the upgrade, pick a matching pair of ghc-version and stack resolver.
+# Two options are provided, one with an empty local cabal.config and the other
+# with a ghc-x.y.z resolver that for stack means packages shipped with the
+# compiler.
+GHC_UPGRADE ?= 9.6.3
+STACKAGE_UPGRADE ?= nightly-2023-11-25
+
+# Imports can be relative to the project or relative to importing file.
+# ImportRelative works with cabal-3.10 and is the default.
+# ProjectRelative works with cabal-3.8.
+CABAL_RELATIVITY ?= ImportRelative
